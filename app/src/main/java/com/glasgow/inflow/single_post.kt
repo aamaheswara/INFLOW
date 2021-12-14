80f34934f3401e58f483aeef8e8341e74f9f07a4
rikobediatra <rikobediatra@gmail.com>
2021-12-14 21:46:06 +0700

    tampilan home dan single post

---
+++ app/src/main/java/com/glasgow/inflow/single_post.kt
@@ -0,0 +1,11 @@
+package com.glasgow.inflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class single_post : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_post)
+    }
+}
