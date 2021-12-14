3a48eafd54c4ef7fdb09efc306442efeda24a75c
aamaheswara <39681286+aamaheswara@users.noreply.github.com>
2021-12-14 22:23:11 +0700

    user profile using bottom nav template

--- app/src/main/java/com/glasgow/inflow/MainActivity.kt
+++ app/src/main/java/com/glasgow/inflow/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.glasgow.inflow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun fToProfile(view: View){
+        val myIntent = Intent(this, UserProfileActivity::class.java)
+        startActivity(myIntent)
     }
 }
