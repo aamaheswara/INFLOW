cd6bf249b37ac1459587e7c0415fd925eb0573ab
aamaheswara <39681286+aamaheswara@users.noreply.github.com>
2021-12-16 17:58:12 +0700

    recyclerview is working from template

--- app/src/main/java/com/glasgow/inflow/MainActivity.kt
+++ app/src/main/java/com/glasgow/inflow/MainActivity.kt
@@ -4,14 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var blogAdapter: BlogRecyclerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+        addDataset()
     }
     fun fToProfile(view: View){
         val myIntent = Intent(this, UserProfileActivity::class.java)
         startActivity(myIntent)
+    }
+
+    private fun addDataset(){
+        val ds = DataSource.createDataSet()
+        blogAdapter.submitList(ds)
+    }
+
+    private fun initRecyclerView(){
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val topSpacingDecorator = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecorator)
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/glasgow/inflow/models/BlogPost.kt
@@ -0,0 +1,21 @@
+package com.glasgow.inflow.models
+
+data class BlogPost(
+
+    var title: String,
+
+    var body: String,
+
+    var image: String,
+
+    var username: String // Author of blog post
+
+
+) {
+
+    override fun toString(): String {
+        return "BlogPost(title='$title', image='$image', username='$username')"
+    }
+
+
+}
