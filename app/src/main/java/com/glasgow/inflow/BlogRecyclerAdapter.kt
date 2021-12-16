5a5d994d5cf70088c3d034cd095e597d447e8e6d
aamaheswara <39681286+aamaheswara@users.noreply.github.com>
2021-12-16 18:26:46 +0700

    color and refactor id

--- app/src/main/java/com/glasgow/inflow/BlogRecyclerAdapter.kt
+++ app/src/main/java/com/glasgow/inflow/BlogRecyclerAdapter.kt
@@ -1,11 +1,9 @@
 package com.glasgow.inflow
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.glasgow.inflow.R.layout.layout_blog_list_item
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.glasgow.inflow.models.BlogPost
@@ -49,9 +47,9 @@ class BlogRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
         itemView: View
     ): RecyclerView.ViewHolder(itemView){
 
-        val blog_image = itemView.blog_image
-        val blog_title = itemView.blog_title
-        val blog_author = itemView.blog_author
+        val reviewPostImage = itemView.review_post_image
+        val reviewPostTitle = itemView.review_post_title
+        val reviewPostAuthor = itemView.review_post_author
 
         fun bind(blogPost: BlogPost){
 
@@ -62,9 +60,9 @@ class BlogRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
             Glide.with(itemView.context)
                 .applyDefaultRequestOptions(requestOptions)
                 .load(blogPost.image)
-                .into(blog_image)
-            blog_title.setText(blogPost.title)
-            blog_author.setText(blogPost.username)
+                .into(reviewPostImage)
+            reviewPostTitle.setText(blogPost.title)
+            reviewPostAuthor.setText(blogPost.username)
 
         }
 
